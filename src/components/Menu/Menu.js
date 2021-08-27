329682cf4f1e21cd1ee541d26b093b5bc69bb7c8
Nicholas Angelo <40876154+Nicholasangelo@users.noreply.github.com>
2021-08-27 14:03:42 -0400

    Use Nbutton
    lint menu and social files

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import '@fontsource/federo';
 import WindowTo from '../WindowTo/WindowTo';
-import { keyDownA11y } from '../../utils';
+import Nbutton from '../../elements/Nbutton/Nbutton';
 
 const fontType = {
     fontFamily: '"Federo", sans-serif',
@@ -15,17 +15,19 @@ const Menu = ({ topics }) => {
         setTopic(e.target.dataset.id);
     };
 
-    const MenuItem = (t) => (
-        <h3
-            style={fontType}
-            className={`nav${topic === t.text ? ' active' : ''}`}
-            data-id={t.name}
-            onClick={keyDownA11y(handleClick)}
-            key={t.name}
-        >
-            {t.text}
-        </h3>
-    );
+    const MenuItem = (t) => {
+        const { text, name } = t;
+        return (
+            <Nbutton
+                text={text}
+                textStyle={fontType}
+                className={`nav${topic === text ? ' active' : ''}`}
+                name={name}
+                handler={handleClick}
+                key={name}
+            />
+        );
+    };
     return (
         <>
             <div className='menu'>{topics.map(MenuItem)}</div>
