75291f29b73702467fb46e7abd193ad1e13cc999
Nicholasangelo <nicholasangelobatten@gmail.com>
2021-12-28 14:35:34 -0500

    remove unused files and maybe fix build error

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-export { default as MainStrip } from './MainStrip/MainStrip.js';
-export { default as Menu } from './Menu/Menu.js';
-export { default as FourOhFour } from './FourOhFour/FourOhFour.js';
-export { default as Social } from './Social/Social.js';
+export { default as MainStrip } from './MainStrip/MainStrip';
+export { default as Menu } from './Menu/Menu';
+export { default as FourOhFour } from './FourOhFour/FourOhFour';
+export { default as Social } from './Social/Social';
 export { default as WindowTo } from './WindowTo/WindowTo';
