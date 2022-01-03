b8ce91928a6b21a6011a1acb455566040f739bba
Nicholas Angelo Batten <40876154+Nicholasangelo@users.noreply.github.com>
2022-01-03 15:05:38 -0500

    Develop (#11)
    * Lint and Util
    
    fixing linting errors and add utils.js
    
    * Use Nbutton
    
    lint menu and social files
    
    * hotfix: mobile and links
    
    * remove unused files and maybe fix build error
    
    * fix naming on FourOhFour page compoenent, ya goof
    
    * bracket import
    
    * Strings (#10)
    
    * adds whoami.strings and constants
    
    * doodles mobile style and linting errors
    
    * remove commented styles

---
+++ src/components/WindowTo/components/whoami/whoami.strings.js
@@ -0,0 +1,13 @@
+export const HAT_TIP =
+    'Practical problem solving without sacrificing an intuitive sense of style and cohesion wither it is shade from the sun or complex UI systems.';
+
+export const STARE_TIP = 'Not bothered.';
+
+export const FLOWER_TIP =
+    'Sensitive. Aware. Utilizies the inspiration of nature for finding creative solutions rather than reinventing what has already been provided.';
+
+export const BEAD_TIP = 'Mindful and well traveled. Able to find peace wiith clear eyes and heart in any situation.';
+
+export const GLASSES_TIP = 'Always prepared at multiple levels and with a fallback to tackle unforseen issues.';
+
+export const JEANS_TIP = 'Casual. Durable. Able to withstand use and gets better with age, understanding, and wear.';
