407c72d90e11f40399247d94b611cdd5439d2c9c
Nicholas Angelo Batten <40876154+Nicholasangelo@users.noreply.github.com>
2022-01-03 14:57:41 -0500

    Strings (#10)
    * adds whoami.strings and constants
    
    * doodles mobile style and linting errors

--- src/components/WindowTo/WindowToStyled.js
+++ src/components/WindowTo/WindowToStyled.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const WindowStyle = styled.section`
+    display: flex;
+    justify-items: center;
+    align-items: center;
     width: 100%;
     height: 100%;
 `;
